0dfcfe30daca5d9181d8bf94ae324d15aef66f92
kevinroyd <kevinroyd163@gmail.com>
2022-04-09 19:48:26 +0400

    控制台日志库的实现

--- log/logger/file_test.go
+++ log/logger/file_test.go
@@ -13,3 +13,17 @@ func TestFIleLogger(t *testing.T) {
 	//	io流操作都需要close
 	logger.Close()
 }
+
+/*
+控制台输出测试函数
+当一个测试用例有多个test时
+*/
+func TestConsoleLogger(t *testing.T) {
+	//	创建文件实例
+	logger := NewConsoleLogger(LogLevelDebug)
+	logger.Debug("the user is first from china as us")
+	logger.Warn("this is warn test")
+	//	io流操作都需要close
+	logger.Close()
+}
+
