a6cea8a62d1e22e81994f85d1d8156400f2e22df
kevinroyd <kevinroyd163@gmail.com>
2022-04-09 18:05:39 +0400

    修改自定义日志库实现方法

--- log/logger/util.go
+++ log/logger/util.go
@@ -8,7 +8,8 @@ import (
 func GetLineInfo()(fileName string,funcName string,lineNo int){
 	// pc 程序执行计数器整数类型
 	// 获取的文件路径和程序路径都为绝对路径，我们只需要最后路径即可
-	pc, file, line, ok := runtime.Caller(0)
+	// Caller为栈的深度 实例化对象调用debug级别日志为第0层，程序函数为第1层
+	pc, file, line, ok := runtime.Caller(1)
 	if ok{
 		fileName = path.Base(file)
 		funcName = path.Base(runtime.FuncForPC(pc).Name())
