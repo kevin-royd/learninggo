6420db225bdc12c01ff34ab7fcb6fc4b022271c2
kevinroyd <kevinroyd163@gmail.com>
2022-04-20 18:54:58 +0400

    日志文件切割

--- log/logger/file_test.go
+++ log/logger/file_test.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"fmt"
 	"testing"
+	"time"
 )
 
 /*
@@ -21,16 +23,17 @@ func TestFIleLogger(t *testing.T) {
 当一个测试用例有多个test时
 */
 func TestConsoleLogger(t *testing.T) {
-	//	创建文件实例
-	m := make(map[string]string,8)
-	m["log_level"] = "DEBUG"
-	logger,err := NewConsoleLogger(m)
-	if err != nil {
-		panic(err)
-	}
-	logger.Debug("the user is first from china as us")
-	logger.Warn("this is warn test")
-	//	io流操作都需要close
-	logger.Close()
+	////	创建文件实例
+	//m := make(map[string]string,8)
+	//m["log_level"] = "DEBUG"
+	//logger,err := NewConsoleLogger(m)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//logger.Debug("the user is first from china as us")
+	//logger.Warn("this is warn test")
+	////	io流操作都需要close
+	//logger.Close()
+	fmt.Println(time.Now().Hour())
 }
 
